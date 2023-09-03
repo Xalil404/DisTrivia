d83c0f5a5c730488317a7d7e112c08006d7c6281
TEST <test@TESTs-MacBook-Pro.local>
2023-09-03 21:10:58 +0100

    Added array for the first question in js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Quiz questions & answers
+const questions = [
+    {
+        question: "How many 'Academy Awards for Best Original Song' has Disney won?",
+        choices: ["14", "18", "22", "30"],
+        correctAnswer: "14"
+      },  
+];
