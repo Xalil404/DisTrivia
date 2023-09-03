10439668ec746cbee86a6dee994e1483fa494050
TEST <test@TESTs-MacBook-Pro.local>
2023-09-03 21:16:44 +0100

    Added sixth array question in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,30 @@ const questions = [
         question: "How many 'Academy Awards for Best Original Song' has Disney won?",
         choices: ["14", "18", "22", "30"],
         correctAnswer: "14"
-      },  
-      {
+    },  
+    {
         question: "Which Disney movie has had the most sequels?",
         choices: ["Toy Story", "Lion King", "Mulan", "Cinderella"],
         correctAnswer: "Toy Story"
-      },  
-      {
+    },  
+    {
         question: "I will Make a Man Out of You is a song from which movie?",
         choices: ["Mulan", "Toy Story", "Lion King", "Snow White"],
         correctAnswer: "Mulan"
-      },  
-      {
+    },  
+    {
         question: "What character have both Glen Close and Emma Stone played?",
         choices: ["Mufasa", "Ariel", "Mulan", "Cruella de Vil"],
         correctAnswer: "Cruella de Vil"
-      },  
-      {
+    },  
+    {
         question: "Who was the first Disney princess?",
         choices: ["Sleeping Beauty", "Ariel", "Snow White", "Cinderella"],
         correctAnswer: "Snow White"
-      },  
+    },  
+    {
+        question: "What color are Mickey Mouse's shoes?",
+        choices: ["Red", "Yellow", "Black", "Pink"],
+        correctAnswer: "Yellow"
+    },
 ];
